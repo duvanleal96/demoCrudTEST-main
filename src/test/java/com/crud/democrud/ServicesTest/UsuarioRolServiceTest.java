29f7764786ecdba91aa65cd8238377a66948dd29
duvanleal96 <duvanleal65@gmail.com>
2022-06-08 23:58:56 -0500

    se implementa el test guardarUsuario
    se crea test y se implementa guardar usuario en la nueva tabla rol

--- src/test/java/com/crud/democrud/ServicesTest/UsuarioRolServiceTest.java
+++ src/test/java/com/crud/democrud/ServicesTest/UsuarioRolServiceTest.java
@@ -1,25 +1,31 @@
 package com.crud.democrud.ServicesTest;
 
+import com.crud.democrud.models.UsuarioModel;
 import com.crud.democrud.models.UsuarioRolModel;
 import com.crud.democrud.repositories.UsuarioRolRepository;
+import com.crud.democrud.services.UsuarioService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import static  org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRolServiceTest {
     @Autowired
     UsuarioRolRepository usuarioRolRepository;
 
     @Test
     public void testGuardarUsuario(){
-        UsuarioRolModel usuarioRolModel=new UsuarioRolModel(1L,1L,"tigre");
-        UsuarioRolModel usuarioModelRegistrado = usuarioRolRepository.save(usuarioRolModel);
-        assertNotNull(usuarioModelRegistrado);
+        UsuarioModel usuarioModel=new UsuarioModel("aquaman","coco@gmail.com",99);
+        UsuarioRolModel usuarioRolModel=new UsuarioRolModel(1L,1L,"aguatero");
+        UsuarioRolModel usuarioRolModelRegistrado = usuarioRolRepository.save(usuarioRolModel);
+        assertNotNull(usuarioRolModelRegistrado);
     }
 
     @Test
